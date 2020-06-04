18643e0ea999ca5ac60d2e08788a62c52f6cec56
André Lins <andrelucas01@hotmail.com>
2020-06-04 12:51:23 -0300

    Getting all controllers

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,11 @@
+import path from "path";
 class Config {
   static PATH_BASE = "./example";
+  static PATH_CONTROLLERS = path.join(
+    process.cwd(),
+    Config.PATH_BASE,
+    "controllers"
+  );
 }
 
 export default Config;
--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,15 +1,1 @@
-import GraphCompiler from "./GraphCompiler";
-
-class Compiler {
-  compile() {
-    const graphqCompiler = new GraphCompiler(
-      "./example/controllers/TestController.ts"
-    );
-
-    graphqCompiler.compile();
-
-    console.log(graphqCompiler.getGraphs());
-  }
-}
-
-export default Compiler;
+export { default } from "./Compiler";
