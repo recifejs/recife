4cb0c39448dfd44b25432756d73dfb012b3078f2
André Lins <andrelucas01@hotmail.com>
2020-06-04 22:44:37 -0300

    Inserting src folder in example

--- example/index.ts
+++
@@ -1,5 +0,0 @@
-import TestController from "./controllers/TestController";
-
-const testController = new TestController();
-
-console.log(testController.getComment);
---
+++ example/src/controllers/TestController.ts
@@ -0,0 +1,20 @@
+import { Query, Mutation } from '../../../src';
+
+import { CommentCreate } from '../inputs/CommentInput';
+
+export default class TestController {
+  @Query()
+  getComment(): String {
+    return 'teste';
+  }
+
+  @Query()
+  getPost(): string {
+    return 'post';
+  }
+
+  @Mutation()
+  createComment(type: CommentCreate) {
+    return 'create';
+  }
+}
