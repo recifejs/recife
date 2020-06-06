72c04a592d038dd986b301df5818a17929cf6dcf
André Lins <andrelucas01@hotmail.com>
2020-06-06 17:58:38 -0300

    New version

--- src/Recife.ts
+++ src/Recife.ts
@@ -1,8 +1,8 @@
 import path from 'path';
 
 class Recife {
-  static PATH_BASE = 'example/src';
-  static PATH_BUILD = path.join(process.cwd(), 'example/dist');
+  static PATH_BASE = 'src';
+  static PATH_BUILD = path.join(process.cwd(), 'dist');
   static PATH_CONTROLLERS = path.join(
     process.cwd(),
     Recife.PATH_BASE,
