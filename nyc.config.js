a105c3599bb05c27615f6675704a542261388330
André Lins <andrelucas01@hotmail.com>
2020-06-12 17:17:34 -0300

    Fix coverage config

--- nyc.config.js
+++ nyc.config.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = {
+  require: ['ts-node/register'],
   include: ['src', 'scripts'],
+  all: true,
   exclude: ['coverage', 'test', 'example'],
   reporter: ['html', 'lcov', 'text-lcov'],
   'report-dir': 'coverage'
