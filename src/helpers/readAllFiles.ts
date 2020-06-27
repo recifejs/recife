d66b2113d56dbbb048f18b14d4e88b2e82ed61fd
André Lins <andrelucas01@hotmail.com>
2020-06-27 19:57:03 -0300

    Using path lib for create path in readAllFiles

--- src/helpers/readAllFiles.ts
+++ src/helpers/readAllFiles.ts
@@ -1,15 +1,18 @@
 import fs from 'fs';
+import path from 'path';
 
-const readAllFiles = (path: string): Promise<string[]> => {
+const readAllFiles = (basePath: string): Promise<string[]> => {
   return new Promise(resolve => {
     let filesResult: string[] = [];
 
-    fs.readdir(path, async (err, files) => {
+    fs.readdir(basePath, async (_err, files) => {
       for (let i = 0; i < files.length; i++) {
-        if (fs.lstatSync(`${path}/${files[i]}`).isFile()) {
-          filesResult.push(`${path}/${files[i]}`);
+        const file = path.join(basePath, files[i]);
+
+        if (fs.lstatSync(file).isFile()) {
+          filesResult.push(file);
         } else {
-          filesResult = filesResult.concat(await readAllFiles(`${path}/${files[i]}`));
+          filesResult = filesResult.concat(await readAllFiles(file));
         }
       }
 
