7fe1cefe635bc9c53d9c049fd5914ce044313913
André Lins <andrelucas01@hotmail.com>
2020-06-28 11:46:45 -0300

    Validate exist folder in readAllFiles function

--- src/helpers/readAllFiles.ts
+++ src/helpers/readAllFiles.ts
@@ -4,19 +4,24 @@ import path from 'path';
 const readAllFiles = (basePath: string): Promise<string[]> => {
   return new Promise(resolve => {
     let filesResult: string[] = [];
+    fs.exists(basePath, exist => {
+      if (exist) {
+        fs.readdir(basePath, async (_err, files) => {
+          for (let i = 0; i < files.length; i++) {
+            const file = path.join(basePath, files[i]);
 
-    fs.readdir(basePath, async (_err, files) => {
-      for (let i = 0; i < files.length; i++) {
-        const file = path.join(basePath, files[i]);
+            if (fs.lstatSync(file).isFile()) {
+              filesResult.push(file);
+            } else {
+              filesResult = filesResult.concat(await readAllFiles(file));
+            }
+          }
 
-        if (fs.lstatSync(file).isFile()) {
-          filesResult.push(file);
-        } else {
-          filesResult = filesResult.concat(await readAllFiles(file));
-        }
+          resolve(filesResult);
+        });
+      } else {
+        resolve([]);
       }
-
-      resolve(filesResult);
     });
   });
 };
